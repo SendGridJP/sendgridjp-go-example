c1cd9f67d8cc7a95092324a3310a8f75eb402ba1
root <root@76503da1ad70.(none)>
2014-04-28 20:22:44 +0000

    Modify main

--- src/main/main.go
+++ src/main/main.go
@@ -2,18 +2,51 @@ package main
 
 import (
     "fmt"
+    "log"
+    "io/ioutil"
     "github.com/sendgrid/sendgrid-go"
+    "encoding/json"
 )
 
+type T struct {
+    SENDGRID_USERNAME string
+    SENDGRID_PASSWORD string
+    TOS []string
+    FROM string
+}
+
 func main() {
-    sg := sendgrid.NewSendGridClient("sendgrid_user", "sendgrid_key")
-    message := sendgrid.NewMail()
-    message.AddTo("yamil@sendgrid.com")
-    message.AddToName("Yamil Asusta")
-    message.AddSubject("SendGrid Testing")
-    message.AddText("WIN")
-    message.AddFrom("yamil@sendgrid.com")
-    if r := sg.Send(message); r == nil {
+
+    b, err_read := ioutil.ReadFile("config.json")
+    if err_read != nil { panic(err_read) }
+
+    t := T{}
+    err := json.Unmarshal(b, &t)
+    if err != nil {
+        log.Fatalf("error: %v", err)
+    }
+
+    email := sendgrid.NewMail()
+    for _, to := range t.TOS {
+        email.AddTo(to)
+    }
+    email.AddFrom(t.FROM)
+    email.AddFromName("送信者名")
+    email.AddSubject("[sendgrid-go-example] フクロウのお名前はfullnameさん")
+    email.AddText("familyname さんは何をしていますか？\r\n 彼はplaceにいます。")
+    email.AddHTML("<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。")
+    sub := make(map[string][]string)
+    sub["fullname"] = []string{"田中 太郎", "佐藤 次郎", "鈴木 三郎"}
+    sub["familyname"] = []string{"田中", "佐藤", "鈴木"}
+    sub["place"] = []string{"office", "home", "office" }
+    email.SetSubstitutions(sub)
+    email.AddSection("office", "中野")
+    email.AddSection("home", "目黒")
+    email.AddCategory("カテゴリ1") 
+    email.AddAttachment("./gif.gif")
+
+    sg := sendgrid.NewSendGridClient(t.SENDGRID_USERNAME, t.SENDGRID_PASSWORD)
+    if r := sg.Send(email); r == nil {
         fmt.Println("Email sent!")
     } else {
         fmt.Println(r)
