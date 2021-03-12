503f4c1abb78a2024362e539e41fd55c2065bb88
kento-yoshida <kento-yoshida.kke.co.jp>
2021-03-12 10:44:21 +0900

    Rewrite-2021_add-test

--- src/main/main.go
+++ src/main/main.go
@@ -1,50 +1,90 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strings"
-    "log"
-    "github.com/sendgrid/sendgrid-go"
-    "github.com/joho/godotenv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"io/ioutil"
+	"encoding/base64"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-    err_read := godotenv.Load()
-    if err_read != nil {
-        log.Fatalf("error: %v", err_read)
-    }
+	err_read := godotenv.Load()
+	if err_read != nil {
+	  log.Fatalf("error: %v", err_read)
+	}
 
-
+	// .envから環境変数読み込み
     API_KEY := os.Getenv("API_KEY")
     TOS := strings.Split(os.Getenv("TOS"), ",")
     FROM := os.Getenv("FROM")
 
-    email := sendgrid.NewMail()
-    email.SMTPAPIHeader.SetTos(TOS) // 宛先はこちらで指定したものが使用される
-    email.AddTo(FROM)               // 実際には使用されない。エラー回避のため記載。
-    email.SetFrom(FROM)
-    email.SetFromName("送信者名")
-    email.SetSubject("[sendgrid-go-example] フクロウのお名前はfullnameさん")
-    email.SetText("familyname さんは何をしていますか？\r\n 彼はplaceにいます。")
-    email.SetHTML("<strong> familyname さんは何をしていますか？</strong><br />彼はplaceにいます。")
-    sub := make(map[string][]string)
-    sub["fullname"] = []string{"田中 太郎", "佐藤 次郎", "鈴木 三郎"}
-    sub["familyname"] = []string{"田中", "佐藤", "鈴木"}
-    sub["place"] = []string{"office", "home", "office" }
-    email.SetSubstitutions(sub)
-    email.AddSection("office", "中野")
-    email.AddSection("home", "目黒")
-    email.AddCategory("category1")
-    file, _ := os.OpenFile("./gif.gif", os.O_RDONLY, 0600)
-    email.AddAttachment("gif.gif", file)
-    defer file.Close()
+	message := mail.NewV3Mail()
+	// 送信元
+	from := mail.NewEmail("", FROM)
+	message.SetFrom(from)
 
-    sg := sendgrid.NewSendGridClientWithApiKey(API_KEY)
-    if r := sg.Send(email); r == nil {
-        fmt.Println("Email sent!")
-    } else {
-        fmt.Println(r)
-    }
+	// 宛先
+	p := mail.NewPersonalization()
+	to := mail.NewEmail("", TOS[0])
+	p.AddTos(to)
+	p.SetSubstitution("%fullname%", "田中 太郎")
+	p.SetSubstitution("%familyname%", "田中")
+	p.SetSubstitution("%place%", "中野")
+	message.AddPersonalizations(p)
+
+	p2 := mail.NewPersonalization()
+	to2 := mail.NewEmail("", TOS[1])
+	p2.AddTos(to2)
+	p2.SetSubstitution("%fullname%", "佐藤 次郎")
+	p2.SetSubstitution("%familyname%", "佐藤")
+	p2.SetSubstitution("%place%", "目黒")
+	message.AddPersonalizations(p2)
+
+	p3 := mail.NewPersonalization()
+	to3 := mail.NewEmail("", TOS[2])
+	p3.AddTos(to3)
+	p3.SetSubstitution("%fullname%", "鈴木 三郎")
+	p3.SetSubstitution("%familyname%", "鈴木")
+	p3.SetSubstitution("%place%", "中野")
+	message.AddPersonalizations(p3)
+	// 件名
+	message.Subject = "[sendgrid-go-example] フクロウのお名前は%fullname%さん"
+	// テキストパート
+	c := mail.NewContent("text/plain", "%familyname% さんは何をしていますか？\r\n 彼は%place%にいます。")
+	message.AddContent(c)
+	// HTMLパート
+	c = mail.NewContent("text/html", "<strong> %familyname% さんは何をしていますか？</strong><br>彼は%place%にいます。")	
+	message.AddContent(c)
+	// カテゴリ
+	message.AddCategories("category1")
+	// カスタムヘッダ
+	message.SetHeader("X-Sent-Using", "SendGrid-API")
+	// 添付ファイル
+	a := mail.NewAttachment()
+	file, _ := os.OpenFile("./gif.gif", os.O_RDONLY, 0600)
+	defer file.Close()
+	data, _ := ioutil.ReadAll(file)
+	data_enc := base64.StdEncoding.EncodeToString(data)
+	a.SetContent(data_enc)
+	a.SetType("image/gif")
+	a.SetFilename("owl.gif")
+	a.SetDisposition("attachment")
+	message.AddAttachment(a)
+
+	client := sendgrid.NewSendClient(API_KEY)
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
 }
