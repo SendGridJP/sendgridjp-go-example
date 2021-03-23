7e7cd9c9aa5f8fe8a94e1f1b6797dc6d9734e60c
kento-yoshida <kento-yoshida.kke.co.jp>
2021-03-23 11:28:35 +0900

    Refactored

--- src/main/main.go
+++ src/main/main.go
@@ -21,15 +21,16 @@ func main() {
 	}
 
 	// .envから環境変数読み込み
-    API_KEY := os.Getenv("API_KEY")
-    TOS := strings.Split(os.Getenv("TOS"), ",")
-    FROM := os.Getenv("FROM")
+	API_KEY := os.Getenv("API_KEY")
+	TOS := strings.Split(os.Getenv("TOS"), ",")
+	FROM := os.Getenv("FROM")
 
 	// メッセージの構築
 	message := mail.NewV3Mail()
 	// 送信元を設定
 	from := mail.NewEmail("", FROM)
 	message.SetFrom(from)
+
 	// 1つ目の宛先と、対応するSubstitutionタグを指定
 	p := mail.NewPersonalization()
 	to := mail.NewEmail("", TOS[0])
@@ -38,6 +39,7 @@ func main() {
 	p.SetSubstitution("%familyname%", "田中")
 	p.SetSubstitution("%place%", "中野")
 	message.AddPersonalizations(p)
+
 	// 2つ目の宛先と、対応するSubstitutionタグを指定
 	p2 := mail.NewPersonalization()
 	to2 := mail.NewEmail("", TOS[1])
@@ -46,6 +48,7 @@ func main() {
 	p2.SetSubstitution("%familyname%", "佐藤")
 	p2.SetSubstitution("%place%", "目黒")
 	message.AddPersonalizations(p2)
+
 	// 3つ目の宛先と、対応するSubstitutionタグを指定
 	p3 := mail.NewPersonalization()
 	to3 := mail.NewEmail("", TOS[2])
@@ -54,6 +57,7 @@ func main() {
 	p3.SetSubstitution("%familyname%", "鈴木")
 	p3.SetSubstitution("%place%", "中野")
 	message.AddPersonalizations(p3)
+
 	// 件名を設定
 	message.Subject = "[sendgrid-go-example] フクロウのお名前は%fullname%さん"
 	// テキストパートを設定
@@ -62,6 +66,7 @@ func main() {
 	// HTMLパートを設定
 	c = mail.NewContent("text/html", "<strong> %familyname% さんは何をしていますか？</strong><br>彼は%place%にいます。")	
 	message.AddContent(c)
+
 	// カテゴリ情報を付加
 	message.AddCategories("category1")
 	// カスタムヘッダを指定
